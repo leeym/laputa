533429b128887b83175b57b81fbdcf8e8ba6f76c
Yen-Ming Lee <leeym@leeym.com>
2018-09-05 14:15:21 -0700

    register LocalDateTimeConverter and ZonedDateTimeConverter

--- laputa-lambda/src/main/java/com/leeym/platform/queryengine/SimpleInstantiatorModule.java
+++ laputa-lambda/src/main/java/com/leeym/platform/queryengine/SimpleInstantiatorModule.java
@@ -2,12 +2,18 @@ package com.leeym.platform.queryengine;
 
 import com.kaching.platform.converters.AbstractInstantiatorModule;
 import com.leeym.platform.converters.LocalDateConverter;
+import com.leeym.platform.converters.LocalDateTimeConverter;
+import com.leeym.platform.converters.ZonedDateTimeConverter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class SimpleInstantiatorModule extends AbstractInstantiatorModule {
   @Override
   protected void configure() {
     registerFor(LocalDate.class).converter(LocalDateConverter.class);
+    registerFor(LocalDateTime.class).converter(LocalDateTimeConverter.class);
+    registerFor(ZonedDateTime.class).converter(ZonedDateTimeConverter.class);
   }
 }
