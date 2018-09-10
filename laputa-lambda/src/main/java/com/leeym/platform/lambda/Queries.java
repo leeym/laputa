d817f485a7c828a25dff92bc5ef0110a606fdcf9
Yen-Ming Lee <leeym@leeym.com>
2018-09-10 13:09:50 -0700

    include com.leeym.platform.lambda in Queries.getAllQueries() so common queries like Help can work

--- laputa-lambda/src/main/java/com/leeym/platform/lambda/Queries.java
+++ laputa-lambda/src/main/java/com/leeym/platform/lambda/Queries.java
@@ -6,11 +6,15 @@ import java.lang.reflect.Modifier;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Queries {
 
+  public static final String COMMON_QUERY_PACKAGE = Queries.class.getPackage().getName();
+
   public Set<Class<? extends Query>> getAllQueries(String prefix) {
-    return new Reflections(prefix).getSubTypesOf(Query.class).stream()
+    return Stream.of(new Reflections(COMMON_QUERY_PACKAGE), new Reflections(prefix))
+      .flatMap(reflections -> reflections.getSubTypesOf(Query.class).stream())
       .filter(aClass -> !Modifier.isAbstract(aClass.getModifiers()))
       .collect(Collectors.toSet());
   }
