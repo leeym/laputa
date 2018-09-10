2b90438c0d061d9108c5f9e4fbe8cd177788a04b
Yen-Ming Lee <leeym@leeym.com>
2018-09-10 12:49:34 -0700

    add AbstractServiceTest in laputa-lambda-test

---
+++ laputa-lambda-test/src/main/java/com/leeym/platform/lambda/AbstractServiceTest.java
@@ -0,0 +1,100 @@
+package com.leeym.platform.lambda;
+
+import com.google.inject.TypeLiteral;
+import com.kaching.platform.converters.InstantiatorModule;
+import org.junit.Test;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+import static com.kaching.platform.converters.Instantiators.createConverter;
+import static com.kaching.platform.converters.Instantiators.createInstantiator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public abstract class AbstractServiceTest {
+
+  public abstract AbstractLambdaProxyHandler getHandler();
+
+  @Test
+  public void queriesArePublic() {
+    getHandler().getAllQueries()
+      .forEach(aClass -> assertTrue(aClass.getName(), Modifier.isPublic(aClass.getModifiers())));
+  }
+
+  @Test
+  public void queriesHaveOnlyOneConstructor() {
+    getHandler().getAllQueries()
+      .forEach(aClass -> assertEquals(aClass.getName(), 1, aClass.getConstructors().length));
+  }
+
+  @Test
+  public void queryConstructorsArePublic() {
+    getHandler().getAllQueries().stream()
+      .flatMap(aClass -> Arrays.stream(aClass.getConstructors()))
+      .forEach(constructor -> assertTrue(Modifier.isPublic(constructor.getModifiers())));
+  }
+
+  @Test
+  public void queryCanBeInstantiated() {
+    InstantiatorModule module = getHandler().getInstantiatorModule();
+    getHandler().getAllQueries().forEach(aClass -> {
+      try {
+        createInstantiator(aClass, module);
+      } catch (Exception e) {
+        throw new RuntimeException("Query [" + aClass.getSimpleName() + "] can not be instantiated", e);
+      }
+    });
+  }
+
+  @Test
+  public void parametersCanBeConverted() {
+    InstantiatorModule module = getHandler().getInstantiatorModule();
+    getHandler().getAllQueries().stream()
+      .flatMap(aClass -> Arrays.stream(aClass.getConstructors()))
+      .flatMap(constructor -> Arrays.stream(constructor.getGenericParameterTypes()))
+      .forEach(type -> {
+        try {
+          createConverter(TypeLiteral.get(type), module);
+        } catch (Exception e) {
+          throw new RuntimeException("Parameter [" + type + "] can not be converted", e);
+        }
+      });
+  }
+
+  @Test
+  public void returnTypesCanBeConverted() {
+    InstantiatorModule module = getHandler().getInstantiatorModule();
+    getHandler().getAllQueries().stream()
+      .map(aClass -> {
+        try {
+          return aClass.getDeclaredMethod(Query.METHOD_NAME).getGenericReturnType();
+        } catch (NoSuchMethodException e) {
+          throw new RuntimeException(e);
+        }
+      })
+      .forEach(type -> {
+        try {
+          createConverter(TypeLiteral.get(type), module);
+        } catch (Exception e) {
+          throw new RuntimeException("Return type [" + type + "] can not be converted", e);
+        }
+      });
+  }
+
+  @Test
+  public void queriesAreTested() {
+    getHandler().getAllQueries().forEach(aClass -> {
+      String name = aClass.getName();
+      try {
+        System.out.println("Looking for tests of " + name);
+        Class.forName(name + "Test");
+      } catch (ClassNotFoundException e) {
+        fail("Query [" + aClass.getSimpleName() + "] is not tested");
+      }
+    });
+  }
+
+
+}
