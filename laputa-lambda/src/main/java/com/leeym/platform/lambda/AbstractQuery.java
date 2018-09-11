1f522e969f03f073dc1d06e4e217ccc44c301a0e
Yen-Ming Lee <leeym@leeym.com>
2018-09-11 04:35:25 -0700

    AbstractQuery can be a Runnable

--- laputa-lambda/src/main/java/com/leeym/platform/lambda/AbstractQuery.java
+++ laputa-lambda/src/main/java/com/leeym/platform/lambda/AbstractQuery.java
@@ -2,12 +2,17 @@ package com.leeym.platform.lambda;
 
 import java.util.concurrent.Callable;
 
-public abstract class AbstractQuery<T> implements Query<T>, Callable<T> {
+public abstract class AbstractQuery<T> implements Query<T>, Callable<T>, Runnable {
 
   public abstract T process();
 
   @Override
   public T call() {
     return process();
+  }
+
+  @Override
+  public void run() {
+    process();
   }
 }
