9e2fadb9fcc43b8f450f07102da3a8a705f71abf
Yen-Ming Lee <leeym@leeym.com>
2018-09-13 10:57:48 -0700

    remove unused import

--- laputa-lambda/src/main/java/com/leeym/platform/lambda/AbstractService.java
+++ laputa-lambda/src/main/java/com/leeym/platform/lambda/AbstractService.java
@@ -1,7 +1,6 @@
 package com.leeym.platform.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Module;
