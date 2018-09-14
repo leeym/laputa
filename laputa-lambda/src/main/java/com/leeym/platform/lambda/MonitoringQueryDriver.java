f29c4caa9a1f09e696ee2d97c515300c8f016baf
Yen-Ming Lee <leeym@leeym.com>
2018-09-14 06:23:39 -0700

    temporarily enable profiling by default

--- laputa-lambda/src/main/java/com/leeym/platform/lambda/MonitoringQueryDriver.java
+++ laputa-lambda/src/main/java/com/leeym/platform/lambda/MonitoringQueryDriver.java
@@ -1,9 +1,6 @@
 package com.leeym.platform.lambda;
 
 import com.leeym.platform.common.DefaultChronograph;
-import com.leeym.platform.common.FakeChronograph;
-
-import static java.lang.Boolean.parseBoolean;
 
 public class MonitoringQueryDriver implements QueryDriver {
 
@@ -15,8 +12,7 @@ public class MonitoringQueryDriver implements QueryDriver {
 
   @Override
   public <T> T invoke(Query<T> query) {
-    query.setChronograph(parseBoolean(query.getRequest().getHeaders().get("Profiling"))
-      ? new DefaultChronograph() : new FakeChronograph());
+    query.setChronograph(new DefaultChronograph());
     return query.getChronograph().time(query.getClass(), Query.METHOD_NAME, () -> delegate.invoke(query));
   }
 }
